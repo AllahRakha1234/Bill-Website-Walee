438b775d41c1058f4cf938a3996ad82bf17614ee
Allah Rakha <allahrakhahsp1234@gmail.com>
2025-01-14 13:58:18 +0500

    admin login model upd

--- BillWebsite/server/models/AdminLogin.js
+++ BillWebsite/server/models/AdminLogin.js
@@ -17,8 +17,10 @@ const adminLoginSchema = new mongoose.Schema({
 adminLoginSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();  // Only hash if password is modified
   try {
+    console.log("before hashing in pre: ", this.password)
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
+    console.log("after hashing in pre: ", this.password)
     next();
   } catch (error) {
     next(error);
