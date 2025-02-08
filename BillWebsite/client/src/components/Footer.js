2113fb8f8964a07a2e34eadc342164067a27668a
Allah Rakha <allahrakhahsp1234@gmail.com>
2025-02-08 22:34:14 +0500

    responsive added

--- BillWebsite/client/src/components/Footer.js
+++ BillWebsite/client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-500 text-white text-center p-2">
+    <footer className="bg-blue-500 text-sm sm:text-medium md:text-lg lg:text-xl text-white text-center p-2">
       Copyright © 2024 National University of Science and Technology - NUST, All Rights Reserved.
     </footer>
   );
