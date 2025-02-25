85120290d2947636a9fff7875debe49c816404cf
Allah Rakha <allahrakhahsp1234@gmail.com>
2025-02-25 16:58:08 +0500

    resp added

--- BillWebsite/client/src/components/AdminPageComponents/ChangePassword.js
+++ BillWebsite/client/src/components/AdminPageComponents/ChangePassword.js
@@ -43,15 +43,15 @@ const ChangePasswordPage = () => {
 
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-400">
-      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg transform transition-all hover:scale-105">
-        <h2 className="text-3xl font-bold text-center text-indigo-600 mb-4">
+      <div className="w-full max-w-md bg-white p-6 lg:p-8 mx-5 md:my-0 rounded-lg shadow-lg transform transition-all hover:scale-105">
+        <h2 className="text-lg md:text-xl lg:text-3xl font-bold text-center text-indigo-600 mb-2 lg:mb-4">
           Change Password
         </h2>
-        <p className="text-center text-gray-500 mb-6">
+        <p className="text-center text-gray-500 text-sm lg:text-lg mb-2 lg:mb-6">
           Update your password to secure your account.
         </p>
         <form onSubmit={handleChangePassword}>
-          <div className="mb-4">
+          <div className="mb-2 md:mb-4">
             <label className="block text-gray-700 font-semibold mb-2">
               Current Password
             </label>
@@ -59,11 +59,11 @@ const ChangePasswordPage = () => {
               type="password"
               value={currentPassword}
               onChange={(e) => setCurrentPassword(e.target.value)}
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
+              className="w-full px-4 py-1 md:py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
               placeholder="Enter current password"
             />
           </div>
-          <div className="mb-6">
+          <div className="mb-4 md:mb-6">
             <label className="block text-gray-700 font-semibold mb-2">
               New Password
             </label>
@@ -71,13 +71,13 @@ const ChangePasswordPage = () => {
               type="password"
               value={newPassword}
               onChange={(e) => setNewPassword(e.target.value)}
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
+              className="w-full px-4 py-1 md:py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
               placeholder="Enter new password"
             />
           </div>
           <button
             type="submit"
-            className="w-full py-2 mb-2 text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 transition duration-200"
+            className="w-full py-2 mb-0 md:mb-2 text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 transition duration-200"
             disabled={isLoading}
           >
             {isLoading ? "Changing..." : "Change Password"}
